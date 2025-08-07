ad035c64e8f1439281112abcf01b02b049d3edf1
ajay panchal <ajaypanchal4534@gmail.com>
2025-08-07 03:06:32 -0800

    Tailwind

--- 03Tailwindprops/src/App.jsx
+++ 03Tailwindprops/src/App.jsx
@@ -2,21 +2,16 @@ import React from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './component/Card'
+import {Card} from './components/card';
 
 function App() {
   //const [count, setCount] = useState(0)
-  let myobj = {
-   username: "hitesh",
-   age: 21
-  }
- //let newArr = [1,2,3]
   return (
     <>
-      <h1 ClassName='bg-green-400 text-blackP-4 rounded-xl mb-4'>Tailwind test</h1>
+      <h1 className='bg-green-400 text-blackP-4 rounded-xl mb-4'>Tailwind test</h1>
       
-    < Card username = "chaiaurcode" SomeObj = {myobj}/>
-    < Card />
+    <Card/>
+    <Card/>
       </>
   )
 }
