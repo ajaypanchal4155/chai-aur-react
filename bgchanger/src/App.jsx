e3afb5d906c106b7234a8a35ce575a029ce12a3b
ajay panchal <ajaypanchal4534@gmail.com>
2025-08-07 20:28:14 -0800

    BGCHANGER

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setcolor] = useState("olive")
+  return(
+    <div className = "w-full h-screen duration-200"
+    style = {{background:color}}
+    >
+      <div className = "fixed flex- wrap justify-centre bottom-12 inset-x-0 px-2">
+      <div className = "flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3*1">
+        <button 
+        onClick = {() => setcolor("red")}
+        className = "outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style = {{backgroundColor:"red"}}
+        >red</button>
+        <button
+        onClick = {() =>setcolor("green")}
+        className = "outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style ={{backgroundColor:"green"}}
+        >Green</button>
+        <button
+        onClick = {()=>setcolor("blue")}
+        className = "outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style = {{backgroundColor:"blue"}}
+        >blue</button>
+      </div>
+      </div>
+    </div>
+  )
+
+}
+export default App
