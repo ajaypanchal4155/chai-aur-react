2c801ec355a8533c60de0f25b4d82da2e8df721b
ajay panchal <ajaypanchal4534@gmail.com>
2025-08-17 01:55:16 -0800

    reactRouter

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,35 @@
+
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+const data = useLoaderData(); 
+    //const [data, setData] = useState([]);
+    //useEffect(()=>{
+    //fetch('https://api.github.com/users/hiteshchoudhary')
+     //.then(response => response.json())
+     //.then(data =>{
+        //console.log(data);
+        //setData(data)
+     //})
+    //},[])
+
+  return (
+    <div className="text-center m-4">
+  <div className="bg-gray-600 text-white p-4 text-3xl">
+    Github followers: {data?.followers}
+  </div>
+  <img
+    className="mx-auto mt-4"
+    src={data?.avatar_url}
+    alt="Git picture"
+    width={300}
+  />
+</div>
+  )
+}
+
+export default Github;
+export const GithubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
