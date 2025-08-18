4f541627b3ec157b56e0288a68244039e9832561
ajay panchal <ajaypanchal4534@gmail.com>
2025-08-18 05:08:14 -0800

    login

---
+++ 08miniContext/src/Context/UserContextProvider.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import UserContext from "./UserContext";
+import Login from "../components/Login";
+import Profile from "../components/Profile";
+function UserContextProvider({ children })
+    {
+  const [user, setUser] = useState(null);
+    {" "}
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+      <Login />
+      <Profile />
+    </UserContext.Provider>
+  );
+}
+
+export default UserContextProvider;
